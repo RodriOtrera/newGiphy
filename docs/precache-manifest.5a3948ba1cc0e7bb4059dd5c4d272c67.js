70f22cb2428869391ec9f90b75616fdc25960fb4
Rodri Otrera <rodrigo.otrerafornes@gmail.com>
2021-02-06 05:32:52 -0300

    listo

---
+++ docs/precache-manifest.5a3948ba1cc0e7bb4059dd5c4d272c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e649e7a901706065cc66a1394bff48c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d090c13241b4ad5917",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d090c13241b4ad5917",
+    "url": "/static/js/main.78761618.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
